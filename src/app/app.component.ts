06fc6f4db91f8c59c7e1911ffa6c4fa0473890c4
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-02-04 17:20:03 +0530

    user logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { SidebarComponent } from "./core/components/sidebar/sidebar.component";
-
+import { AuthServiceService } from './core/services/auth-service.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent, SidebarComponent],
+  imports: [RouterOutlet, LayoutComponent, SidebarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'auth-app';
+  isLoggedIn = false;
+
+  constructor(private authService: AuthServiceService){}
+
+  ngOnInit(){
+    this.authService.currentUser.subscribe(user => {
+      this.isLoggedIn = !!user;
+    })
+  }
+
 }
